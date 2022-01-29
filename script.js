bb8451b24e2784fc8bf70a610413149e1990fd9f
Mauricio <you@example.com>
2022-01-29 14:36:53 -0300

    temas criados no sass

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+//theme selector
+
+const themes = document.getElementsByName('theme')
+
+themes[0].checked = true
+
+themes.forEach(e => {
+    if (e.checked == true) {
+        console.log(e)
+    } else {
+        console.log('s')
+    }
+})
+
+
+//calculator
+
 const numberButtons = document.querySelectorAll("[data-number]")
 const operationButtons = document.querySelectorAll("[data-operation]")
 
@@ -14,6 +31,29 @@ class Calculator {
         this.currentOperandTextElement = currentOperandTextElement
         this.clear()
     }
+
+    formatDisplayNumber(number) {
+        const stringNumber = number.toString();
+    
+        const integerDigits = parseFloat(stringNumber.split(".")[0]);
+        const decimalDigits = stringNumber.split(".")[1];
+    
+        let integerDisplay;
+    
+        if (isNaN(integerDigits)) {
+          integerDisplay = "";
+        } else {
+          integerDisplay = integerDigits.toLocaleString("en", {
+            maximumFractionDigits: 0,
+          });
+        }
+    
+        if (decimalDigits != null) {
+          return `${integerDisplay}.${decimalDigits}`;
+        } else {
+          return integerDisplay;
+        }
+      }
 
     delete() {
         this.currentOperand = this.currentOperand.toString().slice(0, -1)
@@ -65,7 +105,12 @@ class Calculator {
     appendNumber(number) {
         if (this.currentOperand.includes(".") && number === ".") return
 
-        this.currentOperand = `${this.currentOperand}${number.toString()}`
+        if (this.currentOperand.length >= 14) {
+            return
+        }
+        else {
+            this.currentOperand = `${this.currentOperand}${number.toString()}`
+        }
     }
 
     clear() {
@@ -75,10 +120,13 @@ class Calculator {
     }
 
     updateDisplay() {
-        this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation || ''}`
-        this.currentOperandTextElement.innerText = this.currentOperand
-
-    }
+        this.previousOperandTextElement.innerText = `${this.formatDisplayNumber(
+          this.previousOperand
+        )} ${this.operation || ""}`;
+        this.currentOperandTextElement.innerText = this.formatDisplayNumber(
+          this.currentOperand
+        );
+      }
 
 
 }
