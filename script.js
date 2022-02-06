2ac31948ae752f43af10b4bc12d12050bf404a73
Mauricio <you@example.com>
2022-02-06 00:12:02 -0300

    adicionado script para mudança de temas

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 //theme selector
 
 const themes = document.getElementsByName('theme')
-
 themes[0].checked = true
 
-themes.forEach(e => {
-    if (e.checked == true) {
-        console.log(e)
-    } else {
-        console.log('s')
-    }
-})
 
-
-//calculator
+const body = document.querySelector('body')
+const header = document.querySelector('.header')
+const form = document.querySelector('form')
+const display = document.querySelector('.display')
+const keyboard = document.querySelector('.keyboard')
 
 const numberButtons = document.querySelectorAll("[data-number]")
 const operationButtons = document.querySelectorAll("[data-operation]")
@@ -25,6 +20,66 @@ const resetButton = document.querySelector("[data-reset]")
 const previousOperandTextElement = document.querySelector("[previous-operand]")
 const currentOperandTextElement = document.querySelector("[current-operand]")
 
+
+function verifyChecked() {
+    return document.querySelector('input[type="radio"]:checked').value
+}
+
+function addClass(theme) {
+    body.classList.add(`${theme}-bg-main`)
+    header.classList.add(`${theme}-header`)
+    form.classList.add(`${theme}-form`)
+    display.classList.add(`${theme}-display`)
+    keyboard.classList.add(`${theme}-keyboard`)
+    equalButton.classList.add(`${theme}-equal-btn`)
+    deleteButton.classList.add(`${theme}-del-btn`)
+    resetButton.classList.add(`${theme}-reset-btn`)
+
+    operationButtons.forEach(e => e.classList.add(`${theme}-button`))
+    numberButtons.forEach(e => e.classList.add(`${theme}-button`))
+}
+
+function removeClass(theme) {
+    body.classList.remove(`${theme}-bg-main`)
+    header.classList.remove(`${theme}-header`)
+    form.classList.remove(`${theme}-form`)
+    display.classList.remove(`${theme}-display`)
+    keyboard.classList.remove(`${theme}-keyboard`)
+    equalButton.classList.remove(`${theme}-equal-btn`)
+    deleteButton.classList.remove(`${theme}-del-btn`)
+    resetButton.classList.remove(`${theme}-reset-btn`)
+
+    operationButtons.forEach(e => e.classList.remove(`${theme}-button`))
+    numberButtons.forEach(e => e.classList.remove(`${theme}-button`))
+}
+
+function changeTheme() {
+    if (verifyChecked() === 'theme1') {
+        removeClass('t2')
+        removeClass('t3')
+        addClass('t1')
+    } else if (verifyChecked() === 'theme2') {
+        removeClass('t1')
+        removeClass('t3')
+        addClass('t2')
+    } else {
+        removeClass('t1')
+        removeClass('t2')
+        addClass('t3')
+    }
+}
+
+changeTheme()
+
+themes.forEach(e => {
+    e.addEventListener('click', () => {
+        changeTheme()
+    })
+})
+
+
+//calculator
+
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
@@ -34,26 +89,26 @@ class Calculator {
 
     formatDisplayNumber(number) {
         const stringNumber = number.toString();
-    
+
         const integerDigits = parseFloat(stringNumber.split(".")[0]);
         const decimalDigits = stringNumber.split(".")[1];
-    
+
         let integerDisplay;
-    
+
         if (isNaN(integerDigits)) {
-          integerDisplay = "";
+            integerDisplay = "";
         } else {
-          integerDisplay = integerDigits.toLocaleString("en", {
-            maximumFractionDigits: 0,
-          });
+            integerDisplay = integerDigits.toLocaleString("en", {
+                maximumFractionDigits: 0,
+            });
         }
-    
+
         if (decimalDigits != null) {
-          return `${integerDisplay}.${decimalDigits}`;
+            return `${integerDisplay}.${decimalDigits}`;
         } else {
-          return integerDisplay;
+            return integerDisplay;
         }
-      }
+    }
 
     delete() {
         this.currentOperand = this.currentOperand.toString().slice(0, -1)
@@ -121,12 +176,12 @@ class Calculator {
 
     updateDisplay() {
         this.previousOperandTextElement.innerText = `${this.formatDisplayNumber(
-          this.previousOperand
+            this.previousOperand
         )} ${this.operation || ""}`;
         this.currentOperandTextElement.innerText = this.formatDisplayNumber(
-          this.currentOperand
+            this.currentOperand
         );
-      }
+    }
 
 
 }
